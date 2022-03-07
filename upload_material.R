f59f7cf2a67c6b84ef5156d8ee0a80a4bdf5cae5
mcomas <marc.comas@gmail.com>
2022-03-07 12:12:04 +0100

    Material session 3

--- upload_material.R
+++ upload_material.R
@@ -16,3 +16,10 @@ if(!exists(SESSION.1.ZIP)){
   zip(SESSION.1.ZIP, list.files(SESSION.1, full.names = TRUE))
   file.copy(SESSION.1.ZIP, file.path(.destination_path, SESSION.1.ZIP), overwrite = TRUE)
 }
+
+SESSION.3 = "RSTAT-INTRO-SESSION-3"
+SESSION.3.ZIP = paste0(SESSION.3, ".zip")
+if(!exists(SESSION.3.ZIP)){
+  zip(SESSION.3.ZIP, list.files(SESSION.3, full.names = TRUE))
+  file.copy(SESSION.3.ZIP, file.path(.destination_path, SESSION.3.ZIP), overwrite = TRUE)
+}
